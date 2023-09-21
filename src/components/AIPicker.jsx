77c92e686c682972a4845756ae4a0ff1f6cd68af
Brandy CAYPE <brandycype234@gmail.com>
2023-09-21 09:53:37 +0300

    set up aipicker layout

--- src/components/AIPicker.jsx
+++ src/components/AIPicker.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
-
-const AIPicker = () => {
+import React from 'react';
+import CustomButton from './CustomButton';
+const AIPicker = ({prompt,setPrompt,generatingImg,handleSubmit}) => {
   return (
-    <div>AIPicker</div>
+    <div className='aipicker-container'>
+      <textarea className='aipicker-container'
+      placeholder='Ask AI ...'
+      rows={5}
+      value={prompt}
+      onChange={(e)=> setPrompt(e.target.value)}/>
+      <div className='flex flex-wrap gap-3'>
+        { generatingImg ?(
+        <CustomButton 
+        type='outline'
+        title='Asking AI...'
+        customStyles='text-xs'
+        />):(
+          <>
+           <CustomButton
+          type='outline'
+          title='AI LOGO'
+          handleClick={()=>handleSubmit('logo')}
+        customStyles='text-xs'
+          />
+           <CustomButton
+          type='filled'
+          title='AI FULL'
+          handleClick={()=>handleSubmit('full')}
+        customStyles='text-xs'
+          />
+          </>
+         
+          
+        )
+        }
+      </div>
+    </div>
   )
 }
 
